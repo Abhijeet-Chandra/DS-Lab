7ef56656a1321f92794577145dc7a5ecd038c33c
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2025-08-28 17:23:07 +0530

    Update PolynomialLL.c

--- Lab-6/PolynomialLL.c
+++ Lab-6/PolynomialLL.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 typedef struct Node{
     int coeff;
     int exp;
@@ -7,92 +7,92 @@ typedef struct Node{
     struct Node* prev;
 }Node;
 
-Node* head1 = NULL;
-Node* head2 = NULL;
-Node* head3 = NULL;
+Node* head1 = NULL, *head2 = NULL, *tail1 = NULL, *tail2 = NULL;
 
-Node* create_node(int coeff, int exp) {
+Node* createNode(int coeff, int exp) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->coeff = coeff;
     newNode->exp = exp;
-    newNode->next = NULL;
-    newNode->prev = NULL;
+    newNode->next = newNode->prev = NULL;
     return newNode;
 }
-void create_polynomial(Node** head, int coeff, int exp) {
-    Node* newNode = create_node(coeff,exp);
+
+void createPolynomial(int coeff, int exp,Node** head, Node** tail) {
+    Node* newNode = createNode(coeff,exp);
     if (*head==NULL) {
-        *head = newNode;
-        (*head)->prev = NULL;
+        *head = *tail = newNode;
         return;
     }
-    newNode->next = *head;
-    (*head)->prev = newNode;
-    (*head) = newNode;
-    (*head)->prev = NULL;
+    (*tail)->next = newNode;
+    newNode->prev = *tail;
+    *tail = newNode;
 }
-void print_polynomial(Node** head) {
-    Node* temp = *head;
-    while (temp != NULL) {
-        if (temp->next==NULL) {
-            printf("%dx^%d", temp->coeff, temp->exp);
-            break;
+Node* addPolynomials() {
+    Node* temp1 = head1, *temp2 = head2;
+    Node* head3 = NULL, *tail3 = NULL;
+    while (temp1!=NULL&&temp2!=NULL) {
+        if (temp1->exp>temp2->exp) {
+            createPolynomial(temp1->coeff,temp1->exp,&head3, &tail3);
+            temp1 = temp1->next;
         }
-        printf("%dx^%d + ", temp->coeff, temp->exp);
+        else if (temp2->exp>temp1->exp) {
+            createPolynomial(temp2->coeff,temp2->exp,&head3,&tail3);
+            temp2 = temp2->next;
+        }
+        else {
+            createPolynomial(temp1->coeff+temp2->coeff,temp1->exp,&head3,&tail3);
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+    }
+    while (temp1!=NULL) {
+        createPolynomial(temp1->coeff, temp1->exp, &head3, &tail3);
+        temp1 = temp1->next;
+    }
+    while (temp2!=NULL) {
+        createPolynomial(temp2->coeff, temp2->exp, &head3, &tail3);
+        temp2 = temp2->next;
+    }
+    return head3;
+}
+void display(Node**head) {
+    Node*temp = *head;
+    while (temp!=NULL) {
+        printf("%dx^%d", temp->coeff, temp->exp);
         temp = temp->next;
+        if (temp!=NULL)printf(" + ");
     }
     printf("\n");
 }
-void add_polynomials(Node** head1, Node** head2) {
-    if (*head1==NULL) {
-        head3 = *head2;
-        return;
-    }
-    if (*head2==NULL) {
-        head3 = *head1;
-        return;
-    }
-    Node* temp1 = *head1;
-    Node* temp2 = *head2;
-    while (temp1->next!=NULL) {
-        temp1 = temp1->next;
-    }
-    while (temp2->next!=NULL) {
-        temp2 = temp2->next;
-    }
-    while (temp1!=NULL&&temp2!=NULL) {
-        create_polynomial(&head3, temp1->coeff+temp2->coeff, temp1->exp);
-        temp1 = temp1->prev;
-        temp2 = temp2->prev;
-    }
-    while (temp1!=NULL) {
-        create_polynomial(&head3, temp1->coeff, temp1->exp);
-        temp1 = temp1->prev;
-    }
-    while (temp2!=NULL) {
-        create_polynomial(&head3, temp2->coeff, temp2->exp);
-        temp2 = temp2->prev;
-    }
-
-}
 int main() {
-    int degree,coeff;
-    printf("Enter degree of polynomial1: ");
-    scanf("%d", &degree);
-    for (int i=0; i<=degree; i++) {
-        printf("Enter coefficient for x^%d: ", i);
+    printf("Creating polynomial 1:\n");
+    int op,coeff,exp;
+    do {
+        printf("Enter coefficient: ");
         scanf("%d",&coeff);
-        create_polynomial(&head1,coeff,i);
-    }
-    printf("Enter degree of polynomial2: ");
-    scanf("%d", &degree);
-    for (int i=0; i<=degree; i++) {
-        printf("Enter coefficient for x^%d: ", i);
+        printf("Enter exponent: ");
+        scanf("%d",&exp);
+        createPolynomial(coeff,exp,&head1,&tail1);
+        printf("Enter 1 to add more coefficients, Enter 2 to exit.\n");
+        scanf("%d",&op);
+    } while (op!=2);
+
+    printf("Creating polynomial 2:\n");
+    do {
+        printf("Enter coefficient: ");
         scanf("%d",&coeff);
-        create_polynomial(&head2,coeff,i);
-    }
-    print_polynomial(&head1);
-    print_polynomial(&head2);
-    add_polynomials(&head1,&head2);
-    print_polynomial(&head3);
+        printf("Enter exponent: ");
+        scanf("%d",&exp);
+        createPolynomial(coeff,exp,&head2,&tail2);
+        printf("Enter 1 to add more coefficients, Enter 2 to exit.\n");
+        scanf("%d",&op);
+    } while (op!=2);
+    printf("Poly1:\n ");
+    display(&head1);
+    printf("Poly2:\n ");
+    display(&head2);
+    Node* head3 = addPolynomials();
+    printf("Poly3:\n ");
+    display(&head3);
+
 }
