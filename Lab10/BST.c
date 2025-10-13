216499e3b3ee29f0604e7fee66326e247b07bcf6
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2025-10-14 01:05:27 +0530

    Create BST.c

---
+++ Lab10/BST.c
@@ -0,0 +1,149 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+}TreeNode;
+
+TreeNode* createTreeNode(int data) {
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+TreeNode* createBinarySearchTree(TreeNode* root,int data) {
+    if (root==NULL) {
+        TreeNode* newNode = createTreeNode(data);
+        return newNode;
+    }
+    if (data<root->data) {
+        root->left = createBinarySearchTree(root->left,data);
+    }
+    else if (data>root->data) {
+        root->right = createBinarySearchTree(root->right,data);
+    }
+    return root;
+}
+
+void preorder(TreeNode* root) {
+    if (root==NULL) {
+        return;
+    }
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(TreeNode* root) {
+    if (root==NULL) {
+        return;
+    }
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+
+void postorder(TreeNode* root) {
+    if (root==NULL) {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+
+int searchElement(TreeNode* root, int element) {
+    if (root==NULL)return 0;
+    if (element<root->data) return searchElement(root->left,element);
+    if (element>root->data) return searchElement(root->right,element);
+    return 1;
+}
+
+int findMax(TreeNode* root) {
+    TreeNode* temp = root;
+    while (temp->right!=NULL) {
+        temp = temp->right;
+    }
+    return temp->data;
+}
+
+int findMin(TreeNode* root) {
+    TreeNode* temp = root;
+    while (temp->left!=NULL) {
+        temp = temp->left;
+    }
+    return temp->data;
+}
+
+TreeNode* deleteNodeBST(TreeNode* root, int element) {
+    if (root==NULL)return root;
+    if (root->data==element) {
+        //0 child.(leaf Node)
+        if (root->left==NULL&&root->right==NULL) {
+            free(root);
+            return NULL;
+        }
+        //1 child (left child)
+        if (root->left!=NULL&&root->right==NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
+        //1 child (right child)
+        if (root->left==NULL&&root->right!=NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        }
+        //2 children
+        if (root->left!=NULL&&root->right!=NULL) {
+            int minval = findMin(root->right);
+            root->data = minval;
+            root->right = deleteNodeBST(root->right,minval);
+            return root;
+        }
+    }
+    else if (root->data>element) {
+        root->left = deleteNodeBST(root->left,element);
+    }
+    else {
+        root->right = deleteNodeBST(root->right,element);
+    }
+    return root;
+}
+int main() {
+    int n,ele;
+    printf("Enter number of nodes:");
+    scanf("%d",&n);
+    TreeNode* root = NULL;
+    for (int i=0; i<n; i++) {
+        printf("Enter element:");
+        scanf("%d",&ele);
+        root = createBinarySearchTree(root,ele);
+    }
+    printf("Inorder Traversal.\n");
+    inorder(root);
+    printf("\n");
+    printf("Preorder: \n");
+    preorder(root);
+    printf("\n");
+    printf("Postorder: \n");
+    postorder(root);
+    printf("\n");
+    printf("Enter element to search:");
+    scanf("%d",&ele);
+    if (searchElement(root,ele)) {
+        printf("Element found.\n");
+    }
+    else {
+        printf("Element not found.\n");
+    }
+    printf("Max element: %d\n",findMax(root));
+    printf("Min element: %d\n",findMin(root));
+    root = deleteNodeBST(root,40);
+    printf("After deletion.\n");
+    inorder(root);
+}
