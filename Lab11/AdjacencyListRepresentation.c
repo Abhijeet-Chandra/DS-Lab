c0a438f86868a8c5331242368fe30eeff0838ac4
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2025-10-20 02:37:13 +0530

    Improve graph input prompts and structure
    Refactor graph input prompts for clarity and consistency.

--- Lab11/AdjacencyListRepresentation.c
+++ Lab11/AdjacencyListRepresentation.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-
-int numVertex;
-int isDirected;
 typedef struct graphNode {
     int vertex;
     int weight;
     struct graphNode* next;
 }graphNode;
+
+int numVertex;
+int isDirected;
 
 graphNode* createGraphNode(int vertex, int weight) {
     graphNode* newNode = (graphNode*)malloc(sizeof(graphNode));
@@ -17,12 +17,12 @@ graphNode* createGraphNode(int vertex, int weight) {
     return newNode;
 }
 
-void readGraph(struct graphNode* head[]) {
+void readGraph(graphNode* head[]) {
     graphNode* newNode;
-    int vertex,k,weight,neighbour;
+    int vertex, neighbour,k,weight;
     for (int i=0; i<numVertex; i++) {
         graphNode* tail = NULL;
-        printf("Enter Vertex: ");
+        printf("Enter vertex: \n");
         scanf("%d",&vertex);
         if (head[vertex]==NULL)
             head[vertex] = tail = createGraphNode(vertex,-1);
@@ -30,8 +30,8 @@ void readGraph(struct graphNode* head[]) {
         while (tail->next!=NULL) {
             tail = tail->next;
         }
-        printf("Enter number of vertices connected to vertex %d:",vertex);
-        scanf("%d", &k);
+        printf("Enter the number of neighbours for vertex %d: ",vertex);
+        scanf("%d",&k);
         for (int j=0; j<k; j++) {
             printf("Enter vertex number: ");
             scanf("%d",&neighbour);
@@ -41,17 +41,19 @@ void readGraph(struct graphNode* head[]) {
             tail->next = newNode;
             tail = newNode;
             if (!isDirected) {
-                if (head[neighbour] == NULL) {
-                    head[neighbour] = createGraphNode(neighbour, -1);
+                graphNode* revNode = createGraphNode(vertex,weight);
+                if (head[neighbour]==NULL) {
+                    head[neighbour] = createGraphNode(neighbour,-1);
+                    head[neighbour]->next = revNode;
                 }
-                graphNode *revNode = createGraphNode(vertex, weight);
-                revNode->next = head[neighbour]->next;
-                head[neighbour]->next = revNode;
+                else {
+                    revNode->next = head[neighbour]->next;
+                    head[neighbour]->next = revNode;
+                }
             }
         }
     }
 }
-
 void printGraph(struct graphNode* head[]) {
     for (int i = 0; i<numVertex; i++) {
         graphNode* temp = head[i];
@@ -67,20 +69,18 @@ void printGraph(struct graphNode* head[]) {
         printf("\n");
     }
 }
-
 int main() {
     printf("Enter the number of vertices: ");
     scanf("%d",&numVertex);
-    printf("Enter 1 if the graph is directed else enter 0: ");
-    scanf("%d",&isDirected);
+    printf("Enter 1 if graph is directed and enter 0 if its undirected.\n");
+    scanf("%d", &isDirected);
     graphNode* head[numVertex];
-    for (int i=0; i<numVertex; i++) {
+    for (int i = 0; i < numVertex; i++) {
         head[i] = NULL;
     }
     readGraph(head);
     printGraph(head);
 }
-
 //
 // Created by ABHIJEET CHANDRA on 19-10-2025.
 //
