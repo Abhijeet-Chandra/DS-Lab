c90b712662d16de1feb7ebc40ca073e4f9b1a876
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2025-11-10 17:10:56 +0530

    Refactor infixToPrefix.c for clarity and correctness

--- Lab7/infixToPrefix.c
+++ Lab7/infixToPrefix.c
@@ -1,110 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-typedef struct Node {
+typedef struct Node{
     char data;
     struct Node* next;
 }Node;
 
 Node* top = NULL;
 
-Node* createNode(char val) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = val;
-    newNode->next = NULL;
-    return  newNode;
+int isEmpty(){
+    return top==NULL;
 }
 
-void push(char val) {
-    Node* newNode = createNode(val);
-    if (newNode==NULL) {
-        printf("Memory allocation failed.Stack overflow.");
+Node* createNode(char c){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = c;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void push(char c){
+    Node* newNode = createNode(c);
+    if(!newNode){
+        printf("Memory Allocation failed.\n");
         return;
     }
-    if (top==NULL) {
+
+    if(isEmpty()){
         top = newNode;
         return;
     }
+
     newNode->next = top;
     top = newNode;
 }
-char pop() {
-    if (top==NULL) {
-        printf("stack_underflow.\n");
+
+char pop(){
+    if(isEmpty()){
+        printf("Stack underflow.FUCK OFF!");
         return '\0';
     }
-    char val = top->data;
-    Node*t = top;
+    Node* t = top;
+    char returnvalue = top->data;
     top = top->next;
     free(t);
-    return val;
+    return returnvalue;
 }
-char peek() {
-    if (top==NULL) {
-        printf("stack_underflow.\n");
+char peek(){
+    if(isEmpty()){
+        printf("Stack underflow.FUCK OFF!");
         return '\0';
     }
     return top->data;
 }
 
-int isEmpty() {
-    return top==NULL;
-}
-int precedence(char symbol) {
-    switch (symbol) {
-        case '^':
-            return 3;
+int precedence(char symbol){
+    switch(symbol){
+        case '^': return 3;
         case '*':
         case '/':
             return 2;
         case '+':
         case '-':
             return 1;
-        default:
-            return 0;
+        default: return 0;        
     }
 }
 
-int associativity(char symbol) {
-    switch (symbol) {
+int associativity(char symbol){
+    switch(symbol){
         case '*':
         case '/':
         case '+':
         case '-':
             return 1; //L to R
         case '^':
-            return 0; //R to L;
+            return 0; //R to L
         default: return -1;
     }
 }
 
-int main() {
+int main(){
     char infix[100];
-    char prefix[100];
     printf("Enter infix expression: ");
     scanf("%s",infix);
+    //reversing the expression:
 
-    int k  = 0;
     int n = strlen(infix);
-
-    for (int i=0; i<n/2; i++) {
+    for(int i=0; i<n/2; i++){
         char temp = infix[i];
         infix[i] = infix[n-i-1];
         infix[n-i-1] = temp;
     }
 
-    for (int i=0; i<n; i++) {
-        if (infix[i]==')') infix[i] = '(';
-        else if (infix[i]=='(') infix[i] = ')';
+    for(int i=0; i<n; i++){
+        if(infix[i]=='(') infix[i] = ')';
+        else if(infix[i]==')') infix[i] = '(';
     }
-    for (int i=0; i<n; i++) {
-        switch (infix[i]) {
-            case '(':
-                push(infix[i]);
+
+    char prefix[100];
+    int k = 0;
+    for(int i=0; i<strlen(infix); i++){
+        switch(infix[i]){
+            case '(': push(infix[i]);
                 break;
             case ')':
-                while (!isEmpty()&&peek()!='(') {
-                    prefix[k++] = pop();
+                while(!isEmpty()&&peek()!='('){
+                    prefix[k++] = pop();      
                 }
                 pop();
                 break;
@@ -113,38 +116,33 @@ int main() {
             case '/':
             case '+':
             case '-':
-                if (associativity(infix[i])) {
-                    while (!isEmpty()&&precedence(infix[i])<=precedence(peek())) {
+                if(associativity(infix[i])){
+                    while(!isEmpty()&&(precedence(infix[i])<precedence(peek()))){
                         prefix[k++] = pop();
                     }
-                    push(infix[i]);
                 }
-                else {
-                    while (!isEmpty()&&precedence(infix[i])<precedence(peek())) {
+                else{
+                    while(!isEmpty()&&(precedence(infix[i])<=precedence(peek()))){
                         prefix[k++] = pop();
                     }
-                    push(infix[i]);
                 }
+                push(infix[i]);
                 break;
-            default: prefix[k++] = infix[i];
+            default: prefix[k++] = infix[i];        
         }
     }
+    while(!isEmpty()){
+            prefix[k++] = pop();
+        }
+        prefix[k] = '\0';
+        
+        //reversing again:
 
-    while (!isEmpty()) {
-        prefix[k++] = pop();
-    }
-
-    prefix[k] = '\0';
-    for (int i=0; i<k/2; i++) {
+        for(int i=0; i<k/2; i++){
         char temp = prefix[i];
         prefix[i] = prefix[k-i-1];
         prefix[k-i-1] = temp;
     }
 
-
-    printf("prefix: %s",prefix);
+        printf("prefix expression: %s",prefix);
 }
-
-//
-// Created by ABHIJEET CHANDRA on 11-09-2025.
-//
