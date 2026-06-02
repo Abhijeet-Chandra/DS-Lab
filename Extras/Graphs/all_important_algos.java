e862ce9be2521027866a2dd25b3ee6893a3a4cb6
Abhijeet-Chandra <abhijeetutsav@gmail.com>
2026-06-02 16:47:38 +0530

    Update all_important_algos.java

--- Extras/Graphs/all_important_algos.java
+++ Extras/Graphs/all_important_algos.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class a {
+public class all_important_algos {
     static boolean isDirected; // (directed / undirected)
     //adj list creation
     public static HashMap<Integer,ArrayList<Integer>> makeAdjList(){
@@ -70,7 +70,7 @@ public class a {
 
         while(!q.isEmpty()){
             int curr = q.poll();
-            for(int neighbor : adjList.get(curr)){ //visited one by one
+            for(int neighbor : adjList.get(curr)){ // visit all neighbors of current vertex
                 if(!visitedBFS[neighbor]){
                     q.add(neighbor);
                     visitedBFS[neighbor] = true;
@@ -82,12 +82,12 @@ public class a {
 
     //cycle detection using dfs:
     public static boolean dfsCycleDetectionUndirected(HashMap<Integer,ArrayList<Integer>>adjList, int start, boolean [] visitedDFS, int parent) {
-        visitedDFS[start] = true;
+        visitedDFS[start] = true; //mark current vertex as visited
 
-        for(int neighbor : adjList.get(start)){
-            if(neighbor == parent)continue;
+        for(int neighbor : adjList.get(start)){ //check its neighbors 
+            if(neighbor == parent)continue; // ignore the edge leading back to the parent, because every undirected edge appears twice
 
-            if(visitedDFS[neighbor])return true;
+            if(visitedDFS[neighbor])return true; //if the for any of the neighbor, parent is not the current vertex, as well as neighbor is visited already, it means cycle is there
 
             if(dfsCycleDetectionUndirected(adjList,neighbor,visitedDFS,start))return true;
         }
@@ -95,7 +95,7 @@ public class a {
     }
     public static boolean isCyclicDFS_Undirected(HashMap<Integer,ArrayList<Integer>>adjList, boolean [] visitedDFS){
         for(int vertex : adjList.keySet()){
-            if(!visitedDFS[vertex] && dfsCycleDetectionUndirected(adjList,vertex,visitedDFS,-1)){
+            if(!visitedDFS[vertex] && dfsCycleDetectionUndirected(adjList,vertex,visitedDFS,-1)){ //go to each vertex, if its not visited, apply dfs check on it
                 return true;
             }
         }
@@ -104,20 +104,20 @@ public class a {
 
     //cycle detection using bfs:
     public static boolean bfsCycleDetectionUndirected(HashMap<Integer,ArrayList<Integer>>adjList, int start, boolean [] visitedBFS){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{start,-1});
-        visitedBFS[start] = true;
-        while (!q.isEmpty()){
-            int [] pair = q.poll();
-            int source = pair[0];
-            int parent = pair[1];
+        Queue<int[]> q = new LinkedList<>(); //since its bfs, we need a queue, also, in queue, we are keeping node as well as its parent as a pair {node, parent}
+        q.add(new int[]{start,-1}); //intialize the queue with starting vertex, and its parent to be -1
+        visitedBFS[start] = true; //mark it as visited
+        while (!q.isEmpty()){ //while q is not empty
+            int [] pair = q.poll(); //pop out the front most node
+            int source = pair[0]; //pair[0] denotes the actual node
+            int parent = pair[1];//pair [1] denotes the parent node
 
-            for(int neighbor : adjList.get(source)){
-                if(!visitedBFS[neighbor]){
-                    visitedBFS[neighbor] = true;
-                    q.add(new int[]{neighbor,source});
+            for(int neighbor : adjList.get(source)){ //go to each neighbor of current node (source or pair[0]) one by one
+                if(!visitedBFS[neighbor]){ //if its not visited
+                    visitedBFS[neighbor] = true; //mark it as visited
+                    q.add(new int[]{neighbor,source}); //add it to the queue along with its parent, i.e. {neigbor,source}
                 }
-                else if(neighbor != parent){
+                else if(neighbor != parent){ //if its visited, then we have to check whether the parent is neighbor itself, if yes just skip, if not, then return true, that means there is a cycle
                     return true;
                 }
             }
@@ -126,7 +126,7 @@ public class a {
     }
     public static boolean isCyclicBFS_Undirected(HashMap<Integer,ArrayList<Integer>>adjList, boolean [] visitedBFS){
         for(int vertex : adjList.keySet()){
-            if(!visitedBFS[vertex] && bfsCycleDetectionUndirected(adjList,vertex,visitedBFS)){
+            if(!visitedBFS[vertex] && bfsCycleDetectionUndirected(adjList,vertex,visitedBFS)){//go to each vertex, if its not visited, apply bfs check on it
                 return true;
             }
         }
@@ -137,13 +137,13 @@ public class a {
     //using dfs:
     public static boolean dfsCycleDetectionDirected(HashMap<Integer,ArrayList<Integer>>adjList, int start, boolean [] visitedDFS, boolean [] inRecursion){
         visitedDFS[start] = true;
-        inRecursion[start] = true;
+        inRecursion[start] = true;// mark node as visited and currently present in recursion stack
 
         for(int neighbor : adjList.get(start)){
             if(!visitedDFS[neighbor] && dfsCycleDetectionDirected(adjList,neighbor,visitedDFS,inRecursion)) return true;
             else if(inRecursion[neighbor])return  true;
         }
-        inRecursion[start] = false;
+        inRecursion[start] = false;// remove node from recursion stack while backtracking
         return false;
     }
     public static boolean isCyclicDFS_Directed(HashMap<Integer,ArrayList<Integer>>adjList, boolean [] visitedDFS, boolean [] inRecursion){
@@ -152,6 +152,69 @@ public class a {
         }
         return false;
     }
+
+    //Topological sort:
+    //dfs:
+    public static void dfsTopo(HashMap<Integer,ArrayList<Integer>>adjList, int start, boolean [] visitedDFS, Stack<Integer> pop_stack){
+        if(visitedDFS[start]) return; //if already visited then return (base case)
+        visitedDFS[start] = true; //else make the current vertex visited
+
+        for (int neighbor : adjList.get(start)){ //go to all adjacent vertices of current vertex, basically visit all neighbors one by one and apply dfs one each of them if not visited
+            if(!visitedDFS[neighbor]){
+                dfsTopo(adjList, neighbor, visitedDFS, pop_stack);
+            }
+        }
+
+        pop_stack.push(start); // push vertex after processing all descendants reversing this finishing order gives topological ordering
+    }
+    //bfs:
+    public static void bfsTopo(HashMap<Integer,ArrayList<Integer>>adjList, int [] inDegree){
+        Queue<Integer> queue = new LinkedList<>(); //queue to maintain vertices with 0 indegree
+        for(int i = 0; i<inDegree.length; i++){
+            if(inDegree[i]==0)queue.add(i); //if indegree == 0 add it to the queue
+        }
+        while(!queue.isEmpty()){
+            int source = queue.poll(); //pop the vertex at front (remove the vertex)
+            System.out.print(source+" "); //print that vertex
+            for(int neighbor : adjList.get(source)){ //go to all its neighbors one by one and decrease their indegree
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0)queue.add(neighbor); //if indegree of any neighbor becomes 0, add it to the queue
+            }
+        }
+    }
+    public static void topoSort(HashMap<Integer,ArrayList<Integer>> adjList,boolean [] visitedDFS){
+        if(!isDirected){
+            System.out.println("Graphs is not directed!!"); //for topo sort, graph should be directed and acyclic
+            return;
+        }
+
+        //topo using dfs:
+        Stack<Integer> pop_stack = new Stack<>(); //stack to maintain pop order
+        for(int vertex : adjList.keySet()){ //apply dfs on all vertices one by one
+            if(!visitedDFS[vertex])
+                dfsTopo(adjList, vertex, visitedDFS,pop_stack);
+        }
+
+        System.out.println("Topo sort using DFS: ");
+        while(!pop_stack.isEmpty()){ //empty out the stack to get reverse pop order
+            System.out.print(pop_stack.pop()+" ");
+        }
+        System.out.println();
+
+        //topo using bfs:(Kahn's algo):
+        int [] inDegree = new int[visitedDFS.length]; //array to maintain indegree
+        for(int vertex : adjList.keySet()){ //calculate indegree of each vertex
+            for(int neighbor : adjList.get(vertex)){
+                inDegree[neighbor]++;
+            }
+        }
+
+        System.out.println("\nTopo sort using BFS:");
+        bfsTopo(adjList,inDegree);
+
+        //NOTE: // visited array is unnecessary because a node enters the queue  only when its indegree becomes 0, which can happen at most once
+    }
+
 
     public static void main(String[] args) {
         //create adj list:
@@ -168,7 +231,9 @@ public class a {
 
         boolean [] visitedDFS = new boolean[maxVertex + 1];//for dfs
         boolean [] visitedBFS = new boolean[maxVertex + 1];//for bfs
+        boolean [] inRecursion = new boolean[maxVertex+1];
 
+        //TRAVERSALS:
         //dfs
         System.out.println("DFS: ");
         dfs(adjList,0,visitedDFS);
@@ -177,7 +242,7 @@ public class a {
         System.out.println("BFS: ");
         bfs(adjList,0,visitedBFS);
 
-
+        //CYCLE DETECTION:
         if(!isDirected){
             Arrays.fill(visitedDFS,false); //resetting
             Arrays.fill(visitedBFS,false); //resetting
@@ -189,12 +254,20 @@ public class a {
             System.out.println("Cycle: "+(isCyclicBFS_Undirected(adjList, visitedBFS) ? "present" : "absent"));
         }
         else{
-            boolean [] inRecursion = new boolean[maxVertex+1];
+            
             //cycle detection(using dfs):
             System.out.println("Cycle: "+(isCyclicDFS_Directed(adjList, visitedDFS, inRecursion) ? "present" : "absent"));
 
             //cycle detection(using bfs):
 
+        }
+
+        //TOPOLOGICAL SORT:
+        Arrays.fill(visitedDFS,false); //resetting
+        Arrays.fill(inRecursion, false); //resetting
+        if (isDirected && !isCyclicDFS_Directed(adjList,visitedDFS,inRecursion)) { //if its directed and acyclic (DAG)
+            Arrays.fill(visitedDFS,false); //resetting again cuz we used this in cycle detection lol
+            topoSort(adjList,visitedDFS);
         }
     }
 }
